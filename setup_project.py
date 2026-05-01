661d8aa29e2bbb891432c3ff9145645430e1503b
RobbeJacobs1 <robbe.jacobs@student.uantwerpen.be>
2026-05-01 21:23:30 +0200

    Enable Git LFS for large files

--- setup_project.py
+++
@@ -1,8 +0,0 @@
-import shutil
-# This script is used to copy the "Results" directory to "MatLab" and then remove the original "Results" directory. This is necessary because the "Results" directory contains files that 
-# are needed for the MatLab code to run.
-
-src = "Results"
-dst = "MatLab"
-
-shutil.copytree(src, dst, dirs_exist_ok=True)
